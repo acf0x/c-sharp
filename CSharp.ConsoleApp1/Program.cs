e5481d531f374004553e595d1aa1929a9da7eeff
acf0x <alcasfer96@gmail.com>
2024-05-21 11:41:21 +0200

    1st commit c#

---
+++ CSharp.ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharp.ConsoleApp1;
+
+/// <summary>
+/// La clase Program ocntiene el método Main, donde inicia la ejecucción del programa
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Método Main, inicio del programa
+    /// </summary>
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hello, Todos!");
+        Console.Clear();
+    }
+}
