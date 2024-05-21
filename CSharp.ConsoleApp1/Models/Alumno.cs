0d8a10469889f98c48b565e46c0c648df4109d3e
acf0x <alcasfer96@gmail.com>
2024-05-21 12:23:53 +0200

    Updated Nullable setting in csproj, added using statements and a new method DeclaracionVariables in Program.cs, and modified AssemblyInfo.cs. Also, updated binary files and cache files in obj/Debug/net8.0 directory.

---
+++ CSharp.ConsoleApp1/Models/Alumno.cs
@@ -0,0 +1,13 @@
+namespace CSharp.ConsoleApp1.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Alumno
+    {
+        public string Nombre; // Hay que poner PUBLIC delante porque si no no puede acceder al objeto. Por defecto es privado.
+        public string Apellidos;
+        public int Edad;
+        
+    }
+}
--- CSharp.ConsoleApp1/Program.cs
+++ CSharp.ConsoleApp1/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace CSharp.ConsoleApp1;
+﻿using System.Security.Cryptography;
+using CSharp.ConsoleApp1.Models;
+
+namespace CSharp.ConsoleApp1;
 
 /// <summary>
 /// La clase Program ocntiene el método Main, donde inicia la ejecucción del programa
 /// </summary>
+
 class Program
+
 {
     /// <summary>
     /// Método Main, inicio del programa
@@ -13,5 +18,47 @@ class Program
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Hello, Todos!");
         Console.Clear();
+        DeclaracionVariables();
     }
+
+/// <summary>
+/// Declaracción de variables
+/// </summary>
+    static void DeclaracionVariables()
+    {
+        string texto = "Hola Mundo !!";
+        string otroTexto;
+        System.String texto2 = "Mi nombre es Borja";
+
+        int numero = 10;
+        int otroNumero;
+        System.Int32 numero2 = 0;
+
+        decimal a, b, c;
+    
+
+    // Instanciamos un objeto Alumno y modificamos sus propiedades o variables
+        Alumno alumno = new Alumno();
+        alumno.Nombre = "Borja";
+        alumno.Apellidos = "Sanz";
+        alumno.Edad = 33;
+
+    // Instanciamos un objeto Alumno y asignamos valores a sus propiedades o variables
+        Alumno alumno1 = new Alumno()
+        {
+            Nombre = "Julian",
+            Apellidos = "Sánchez",
+            Edad = 25
+        };
+
+        // Instanciamos un objeto Alumno usando VAR, OBJECT, DYNAMIC
+        // Las variables con tipo implícito (var) se deben inicializar (dar valor)
+        var alumno2 = 10;
+        Console.WriteLine(alumno2.GetType());
+    }
+
+
+
+
+
 }
