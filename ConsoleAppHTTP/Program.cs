a5dd16cbc49ab348801c2e925976b95d6e340fe5
acf0x <alcasfer96@gmail.com>
2024-06-03 14:10:51 +0200

    HTTTP exercises

--- ConsoleAppHTTP/Program.cs
+++ ConsoleAppHTTP/Program.cs
@@ -1,8 +1,15 @@
-﻿namespace ConsoleAppHTTP
+﻿using Newtonsoft.Json;
+using System.Dynamic;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text;
+
+namespace ConsoleAppHTTP
 {
     internal class Program
     {
-        private static HttpClient http = new HttpClient();
+        private static HttpClient http = new HttpClient();          // cada vez que se instancia, bloquea un puerto para él
 
         static void Main(string[] args)
         {
@@ -18,19 +25,143 @@
             http.DefaultRequestHeaders.Add("User-Agent", "HttpCliente .NET Core Demo");
             http.DefaultRequestHeaders.Add("X-Data-1", "Demo");
             http.DefaultRequestHeaders.Add("X-Data-2", "1234567890");
+            //http.DefaultRequestHeaders.Add("Accept", "application/json");
+            //http.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            Get();
+            Get2();
         }
 
+        /// <summary>
+        /// Si solo hay que invocar a la url y obtener resultado:
+        /// </summary>
         static void Get()
         {
+            // get
+            HttpResponseMessage response = http.GetAsync("get?param1=hola").Result;    //sería lo mismo que http.GetAsync("https://postman-echo.com/get?param1=hola").Result, si no hubiesemos añadido BaseAdress
+            
+            // Opción 1, preguntamos por un código de estado concreto (ok = 200, notfound = 404...)
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                // Lectura del cuerpo del mensaje de respuesta como STRING
+                // El texto obtenido normalmente estará en JSON
+                string responseBodyText = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine("");
+                Console.WriteLine("Body:");
+                Console.WriteLine("==============================================");
+                Console.WriteLine($"{responseBodyText}");
+                
+                // Convertir el JSON en Objeto
+                // Utilizamos DYNAMIC cuando no tenemos una clase que represente el objeto retornado
+                var obj = JsonConvert.DeserializeObject<dynamic>(responseBodyText);  //<-- en los < > se pone el tipo de obj (string) o la CLASE (Customer / dynamic...)
 
-            HttpResponseMessage response = http.GetAsync("get").Result;    //sería lo mismo que http.GetAsync("https://postman-echo.com/get").Result, si no hubiesemos añadido BaseAdress
+                Console.WriteLine("");
+                Console.WriteLine("Datos del Body:");
+                Console.WriteLine("==============================================");
+                Console.WriteLine($"URL: {obj["url"]}");
+                Console.WriteLine($"Param 1: {obj["args"]["param1"]}");
+                Console.WriteLine($"Data 1: {obj["headers"]["x-data-1"]}");
+
+                Console.WriteLine($"URL: {obj.url}");
+                Console.WriteLine($"Param 1: {obj.args.param1}");
+                // Console.WriteLine($"Data 1: {obj.headers.x-data-1}"); <-- no es posible al llevar -
+
+                Console.WriteLine("");
+                Console.WriteLine("Headers:");
+                Console.WriteLine("==============================================");
 
 
+                // También tenemos acceso a las cabeceras del mensaje de respuesta
+                foreach(var header in response.Headers)
+                {
+                    Console.WriteLine($"{header.Key}: {header.Value.FirstOrDefault()}");
+                }
 
+                Console.WriteLine($"Date: {response.Headers.GetValues("Date").FirstOrDefault()}");
+
+                IEnumerable<string> valor;
+                response.Headers.TryGetValues("Content-Type", out valor);
+                Console.WriteLine($"Content-Type: {(valor == null ? "" : valor.FirstOrDefault())}");
+
+                var responseHeaders = response.Headers;
+            }
+            else Console.WriteLine($"Error: {response.StatusCode}");
+
+            // Opción 2, preguntamos si finaliza con cualquier código 2XX
+
+            if (response.IsSuccessStatusCode) 
+            {
+                // aquí iría el mismo código que la opción 1
+            }
+            else Console.WriteLine($"Error: {response.StatusCode}");
         }
 
+        /// <summary>
+        /// Este si hay que añadir contenido o cabeceras específicas:
+        /// </summary>
+        static void Get2()
+        {
+            //HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, "get?param1=hola");
+
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Method = HttpMethod.Get;
+            request.RequestUri = new Uri("https://postman-echo.com/get?param1=hola"); // por alguna razón, aquí no vale poner get, tiene que ser entera´, arriba sí deja
 
 
+            // Opcionalmente definimos cabeceras del mensaje
+            // Las cabeceras se anexan a las cabeceras por defecto del objeto HttpClient
+            request.Headers.Add("X-Data-3", "ABC");
+            request.Headers.Add("Accept", "application/json");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Opcionalmente definimos el cuerpo del mensaje
+
+            //Opción 1
+            //request.Content = new StringContent(JsonConvert.SerializeObject(obj2), Encoding.UTF8, "application/json");
+
+            // Opción 2
+            var obj2 = new { Nombre = "Borja", Apellido1 = "Cabeza", Apellido2 = "Rozas" };
+            string obj2JSON = JsonConvert.SerializeObject(obj2);
+            StringContent contenido = new StringContent(obj2JSON, Encoding.UTF8, "application/json");
+
+            request.Content = contenido;
+
+            // Enviamos la petición y obtenemos la respuesta
+
+            HttpResponseMessage response = http.SendAsync(request).Result;
+        }
+
+        /// <summary>
+        /// Muy simple, del msj de respuesta solo quiero el contenido(Body) del resultado, ni siquiera las headers
+        /// </summary>
+        //static void Get3()
+        //{
+        //    try
+        //    {
+        //        var obj3 = http.GetFromJsonAsync<ExpandoObject>("get?param1=hola").Result;
+        //        //if(obj3 != null) 
+        //        //{
+        //        //    Console.WriteLine("");
+        //        //    Console.WriteLine("Datos del Body:");
+        //        //    Console.WriteLine("==============================================");
+        //        //    Console.WriteLine($"URL: {obj3["url"]}");
+        //        //    Console.WriteLine($"Param 1: {obj3["args"]["param1"]}");
+        //        //    Console.WriteLine($"Data 1: {obj3["headers"]["x-data-1"]}");
+
+        //        //    Console.WriteLine($"URL: {obj3.url}");
+        //        //    Console.WriteLine($"Param 1: {obj3.args.param1}");
+        //        }
+        //        else 
+        //        {
+        //            Console.WriteLine($"No se puede acceder a la información"); 
+        //        }
+        //    }
+        //    catch (Exception e) 
+        //    {
+        //        Console.WriteLine($"Error: {e.Message}");
+        //    }
+        //}
 
     }
 }
---
+++ Ejercicios.ConsoleAppHTTP/Program.cs
@@ -0,0 +1,205 @@
+﻿using static System.Net.WebRequestMethods;
+using Newtonsoft.Json;
+using System.Dynamic;
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Ejercicios.ConsoleAppHTTP
+{
+    internal class Program
+    {
+        private static HttpClient http = new HttpClient();
+        static void Main(string[] args)
+        {
+            //ConsultarCodigoPostal();
+            //ConsultarCodigoPostal2();
+            ConsultarIP();
+            ConsultarIP2();
+
+        }
+
+        // Método GET
+        // URL: api.zippopotam.us/es/11540
+
+        static void ConsultarCodigoPostal()
+        {
+            Console.Clear();
+            Console.Write("Escribe un Código Postal: ");
+            string code = Console.ReadLine();
+
+            http.BaseAddress = new Uri($"https://api.zippopotam.us/es/{code}");
+            HttpResponseMessage response = http.GetAsync("").Result;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string responseBodyText = response.Content.ReadAsStringAsync().Result;
+
+                var obj = JsonConvert.DeserializeObject<dynamic>(responseBodyText);
+
+                Console.WriteLine("");
+                Console.WriteLine("Datos:");
+                Console.WriteLine("==============================================");
+                Console.WriteLine($"CP: {obj["post code"]}");
+                Console.WriteLine($"País: {obj["country"]}");
+                Console.WriteLine($"");
+                foreach (var item in obj["places"])
+                {
+                    Console.WriteLine($"Ciudad: {item["place name"]}");
+                    Console.WriteLine($"Comunidad Autónoma: {item["state"]}");
+                    Console.WriteLine($"Longitud: {item["longitude"]}");
+                    Console.WriteLine($"Latitud: {item["latitude"]}");
+                    Console.WriteLine($"");
+                }
+            }
+            else Console.WriteLine($"Error: {response.StatusCode}");
+        }
+
+        static void ConsultarCodigoPostal2()    // Una vez hechas las 2 clases después, puede hacerse así:
+        {
+            Console.Clear();
+            Console.Write("Escribe un Código Postal: ");
+            string code = Console.ReadLine();
+
+            http.BaseAddress = new Uri($"https://api.zippopotam.us/es/{code}");
+            HttpResponseMessage response = http.GetAsync("").Result;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string responseBodyText = response.Content.ReadAsStringAsync().Result;
+
+                var obj = JsonConvert.DeserializeObject<PostalCodeInfo>(responseBodyText);
+
+                Console.WriteLine("");
+                Console.WriteLine("Datos:");
+                Console.WriteLine("==============================================");
+                Console.WriteLine($"CP: {obj.PostCode}");
+                Console.WriteLine($"País: {obj.Country}");
+                Console.WriteLine($"PaísAbreviado: {obj.CountryAbbreviation}");
+                Console.WriteLine($"");
+                foreach (var item in obj.Places)
+                {
+                    Console.WriteLine($"Ciudad: {item.PlaceName}");
+                    Console.WriteLine($"Comunidad Autónoma: {item.State}");
+                    Console.WriteLine($"Comunidad Autónoma Abreviado: {item.StateCode}");
+                    Console.WriteLine($"Longitud: {item.Longitude}");
+                    Console.WriteLine($"Latitud: {item.Longitude}");
+                    Console.WriteLine($"");
+                }
+            }
+            else Console.WriteLine($"Error: {response.StatusCode}");
+        }
+
+        static void ConsultarCodigoPostal3()    // Una vez hechas las 2 clases después, puede hacerse así:
+        {
+            Console.Clear();
+            Console.Write("Escribe un Código Postal: ");
+            string code = Console.ReadLine();
+
+            try
+            {
+                var data = http.GetFromJsonAsync<PostalCodeInfo>($"https://api.zippopotam.us/es/{code}").Result;
+                Console.WriteLine($"CP: {data.PostCode}");
+
+                foreach (var place in data.Places)
+                    Console.WriteLine($" -> {place.PlaceName} - {place.State} ({place.StateCode}) ");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e}");
+            }
+        }
+        static void ConsultarIP()    // Una vez hechas las 2 clases después, puede hacerse así:
+        {
+            Console.Write("Escribe la IP: ");
+            string code2 = Console.ReadLine();
+
+            http.BaseAddress = new Uri($"http://ip-api.com/json/{code2}");
+            HttpResponseMessage response = http.GetAsync("").Result;
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string responseBodyText = response.Content.ReadAsStringAsync().Result;
+
+                var data2 = JsonConvert.DeserializeObject<IP>(responseBodyText);
+
+                Console.WriteLine($"");
+                Console.WriteLine($"Método 1:");
+                Console.WriteLine($"");
+                Console.WriteLine($" -> {data2.city}, {data2.regionName} ({data2.country}) - {data2.org}, {data2.nombre} - LAT: {data2.lat}, LON: {data2.lon}");
+                Console.WriteLine();
+            }
+            else Console.WriteLine($"Error: {response.StatusCode}");
+        }
+        static void ConsultarIP2()    // Una vez hechas las 2 clases después, puede hacerse así:
+        {
+            Console.Write("Escribe la IP: ");
+            string code3 = Console.ReadLine();
+            try
+            {
+                var data3 = http.GetFromJsonAsync<IP>($"http://ip-api.com/json/{code3}").Result;
+                Console.WriteLine($"");
+                Console.WriteLine($"Método 2:");
+                Console.WriteLine($"");
+                Console.WriteLine($"IP: {data3.query}");
+
+                Console.WriteLine($" -> {data3.city}, {data3.regionName} ({data3.country}) - {data3.org}, {data3.nombre} - LAT: {data3.lat}, LON: {data3.lon}");
+                Console.WriteLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error: {e}");
+            }
+        }
+
+
+        public class PostalCodeInfo
+        {
+            [JsonProperty("post code")] // Los JSON estos lo que hacen es decir "lo del objeto PostCode me lo vuelcas en (post code), por si sale null
+            [JsonPropertyName("post code")] // Este es para el Consultar3
+            public string PostCode { get; set; }
+            public string Country { get; set; }
+
+            [JsonProperty("country abbreviation")]
+            [JsonPropertyName("country abbreviation")]
+            public string CountryAbbreviation { get; set; }
+            public List<PostalCodePlace> Places { get; set; }
+        }
+
+        public class PostalCodePlace
+        {
+            [JsonProperty("place name")]
+            [JsonPropertyName("place name")]
+            public string PlaceName { get; set; }
+            public string Longitude { get; set; }
+            public string Latitude { get; set; }
+            public string State { get; set; }
+            [JsonProperty("state abbreviation")]
+            [JsonPropertyName("state abbreviation")]
+            public string StateCode { get; set; }
+        }
+
+        public class IP
+        {
+            public string status { get; set; }
+            public string city { get; set; }
+            public string region { get; set; }
+            public string regionName { get; set; }
+            public string country { get; set; }
+            public string countryCode { get; set; }
+            public string zip { get; set; }
+            public decimal lat { get; set; }
+            public decimal lon { get; set; }
+            public string timezone { get; set; }
+            public string isp { get; set; }
+            public string org { get; set; }
+            public string query { get; set; }
+            [JsonProperty("as")]
+            [JsonPropertyName("as")]
+            public string nombre { get; set; }
+        }
+    }
+}
